8e4794714b8824e77c69427b8f88ca87b4e13b79
Daniela Tumbrägel <daniela.tumbraegel@posteo.de>
2019-04-25 19:55:36 +0200

    added simple task board (tutorial-based)

---
+++ client/src/components/Tasks.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-10">
+        <h1>Tasks</h1>
+        <hr><br><br>
+        <button type="button" class="btn btn-success btn-sm">Add Task</button>
+        <br><br>
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Due</th>
+              <th scope="col">Importance</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Buy cookies</td>
+              <td>Monday, 29th April</td>
+              <td>Very High</td>
+              <td>
+                <button type="button" class="btn btn-warning btn-sm">Update</button>
+                <button type="button" class="btn btn-danger btn-sm">Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Ping from '@/components/Ping';
+import Tasks from '@/components/Tasks';
 
 Vue.use(Router);
 
@@ -8,8 +9,14 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Tasks',
+      component: Tasks,
+    },
+    {
+      path: '/ping',
       name: 'Ping',
       component: Ping,
     },
   ],
+  mode: 'history',
 });
