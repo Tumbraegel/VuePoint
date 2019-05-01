22ec44a8b0df512cefdd6785a7aa224e6ce79407
Daniela Tumbrägel <daniela.tumbraegel@posteo.de>
2019-05-01 16:26:49 +0200

    minor changes

--- client/src/components/Tasks.vue
+++ client/src/components/Tasks.vue
@@ -2,32 +2,63 @@
   <div class="container">
     <div class="row">
       <div class="col-sm-10">
-        <h1>Tasks</h1>
+        <h1>VuePoint</h1>
         <hr><br><br>
         <button type="button" class="btn btn-success btn-sm">Add Task</button>
         <br><br>
         <table class="table table-hover">
           <thead>
             <tr>
-              <th scope="col">Title</th>
-              <th scope="col">Due</th>
-              <th scope="col">Importance</th>
-              <th></th>
+              <th scope="col">Monday</th>
+              <th scope="col">Tuesday</th>
+              <th scope="col">Wednesday</th>
+              <th scope="col">Thursday</th>
+              <th scope="col">Friday</th>
+              <th scope="col">Weekend</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Buy cookies</td>
-              <td>Monday, 29th April</td>
-              <td>Very High</td>
-              <td>
-                <button type="button" class="btn btn-warning btn-sm">Update</button>
-                <button type="button" class="btn btn-danger btn-sm">Delete</button>
-              </td>
-            </tr>
+          <tr>
+            <td v-for="(task, index) in tasks[0]" :key="index">{{ task.title }}</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td v-for="(task, index) in tasks[1]" :key="index">{{ task.title }}</td>
+            <td></td>
+          </tr>
           </tbody>
         </table>
+        <button type="button" class="btn btn-warning btn-sm">Update</button>
+        <button type="button" class="btn btn-danger btn-sm">Delete</button>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    getTasks() {
+      const path = 'http://localhost:5000/tasks';
+      axios.get(path)
+        .then((res) => {
+          this.tasks = res.data.tasks;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getTasks();
+  },
+};
+</script>
--- server/app.py
+++ server/app.py
@@ -15,11 +15,35 @@ app.config.from_object(__name__)
 # enable Cross Origin Resource Sharing (CORS)
 CORS(app)
 
+TASKS = [
+    [{
+        'title': 'Office Party',
+        'description': 'Buy Cookies',
+        'importance':'high'
+    }],
+    [{
+        'title': 'Meeting',
+        'description': 'with Jacob from HR',
+        'importance':'high'
+    },
+    {
+        'title': 'Coffee Date',
+        'description': '@Starbucks',
+        'importance':'low'
+    }]
+]
 
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
+
+@app.route('/tasks', methods=['GET'])
+def all_tasks():
+    return jsonify({
+        'status': 'success',
+        'tasks': TASKS
+    })
 
 
 if __name__ == '__main__':
