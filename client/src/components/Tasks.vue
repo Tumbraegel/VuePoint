a51c78d7348afe4d626f552ef8d29c7a6d4d084e
Dani <daniela.tumbraegel@posteo.de>
2019-05-14 16:16:04 +0200

    added static modal component template for task detail view

--- client/src/components/Tasks.vue
+++ client/src/components/Tasks.vue
@@ -4,35 +4,51 @@
       <div class="col-sm-10">
         <h1>VuePoint</h1>
         <hr><br><br>
-        <button type="button" class="btn btn-success btn-sm">Add Task</button>
+        <button type="button" class="btn btn-success btn-sm" @click="openAddModal()">
+          Add Task
+        </button>
         <br><br>
 
-        <!--
-        <table class="table table-hover">
-          <thead>
-            <tr>
-              <th scope="col" v-for="day in days">{{ day }}</th>
-            </tr>
-          </thead>
-          <tbody>
-          <tr>
-            <td v-for="task in tasks" v-bind:key="task.id">{{ task.title }}</td>
-          </tr>
-          </tbody>
-        </table>
-        -->
+        <!--ADD TASK MODAL-->
+        <div id="wrapper" class="container">
+          <modal v-if="showAddModal">
+            <h3 slot="header" class="modal-title">Add Task</h3>
+            <div slot="footer">
+              <button type="button" class="btn btn-outline-info" @click="closeModal()">
+                Close</button>
+              <button type="button" class="btn btn-primary" data-dismiss="modal"
+              @click="submitAndClose()">Submit</button>
+            </div>
+          </modal>
+        </div>
 
         <div class="weekday-table">
           <li class="weekday" v-for="day in days" :key="day">
             <h2 class="weekday-header">{{ day }}</h2>
             <ul class="task">
-              <li v-for="task in getTasks(day)" :key="task.id">
+              <li id="task" v-for="task in getTasks(day)" :key="task.id" @click="openEditModal()">
                 <strong>{{ task.title }}</strong>
                 <div>{{ task.description }}</div>
               </li>
             </ul>
           </li>
         </div>
+
+        <!--EDIT TASK MODAL-->
+        <div id="wrapper" class="container">
+          <modal v-if="showEditModal">
+            <h3 slot="header" class="modal-title">
+              Edit Task #
+            </h3>
+            <div slot="footer">
+              <button type="button" class="btn btn-outline-info" @click="closeModal()">
+                Close</button>
+              <button type="button" class="btn btn-primary" data-dismiss="modal"
+              @click="submitAndClose()">Submit</button>
+            </div>
+          </modal>
+        </div>
+
         <br>
         <button type="button" class="btn btn-warning btn-sm">Update</button>
         <button type="button" class="btn btn-danger btn-sm">Delete</button>
@@ -61,6 +77,13 @@
   margin: 0px;
 }
 
+#task:hover, task:focus, task:active{
+  background: rgb(51, 153, 95, 0.9);
+  color: rgb(233, 233, 233);
+  cursor: pointer;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.6);
+}
+
 .weekday {
   font-size: 14px;
   display: table-cell;
@@ -74,9 +97,15 @@
 .btn-success {
   background-color: seagreen;
 }
- </style>
+
+.btn-success:hover {
+  background-color:rgb(38, 115, 71);
+}
+</style>
 
 <script>
+import Modal from './Modal';
+
 export default {
   data() {
     return {
@@ -109,17 +138,45 @@ export default {
         date: 'Wednesday',
         description: 'To-Do: Tickets #73, #75, #79',
         importance: 'high',
+      },
+      {
+        id: '5',
+        title: 'Sprint Review',
+        date: 'Wednesday',
+        description: 'Having an existential crisis',
+        important: 'high',
       }],
+      showAddModal: false,
+      showEditModal: false,
     };
   },
+
+  components: {
+    Modal,
+  },
+
   computed: {
     localTasks() {
       return this.tasks;
     },
   },
+
   methods: {
     getTasks(day) {
       return this.localTasks.filter(task => task.date === day);
+    },
+    openAddModal() {
+      this.showAddModal = true;
+    },
+    openEditModal() {
+      this.showEditModal = true;
+    },
+    closeModal() {
+      this.showEditModal = false;
+      this.showAddModal = false;
+    },
+    submitAndClose() {
+    // add stuff
     },
   },
 };
